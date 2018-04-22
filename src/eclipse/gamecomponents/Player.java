b0e7a5371688854d56ef9d47ab21193868fad613
Alex Yang <alex.j.sheng.yang@gmail.com>
2018-04-22 17:56:40 -0400

    diagonal movement

--- src/eclipse/gamecomponents/Player.java
+++ src/eclipse/gamecomponents/Player.java
@@ -16,10 +16,27 @@ public class Player extends GameObject {
 	y = 300;
     }
 
-    @Override
+//    @Override
     public void update(GraphicsContext gc) {
 	gc.clearRect(0, 0, 800, 600);
 	gc.drawImage(SPRITE, x, y, 50, 50);
     }
 
+    public void move(boolean[] directions) {
+        if (directions[0]) {
+            moveUp();
+        }
+
+        if (directions[1]) {
+            moveDown();
+        }
+
+        if (directions[2]) {
+            moveLeft();
+        }
+
+        if (directions[3]) {
+            moveRight();
+        }
+    }
 }
