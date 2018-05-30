1991eb4f6d952bb4c4789f5b5ff88b902253314f
Alex Yang <alex.j.sheng.yang@gmail.com>
2018-05-30 15:42:12 -0400

    Store file internally before starting game

--- src/eclipse/gui/LevelReader.java
+++ src/eclipse/gui/LevelReader.java
@@ -7,6 +7,7 @@ import eclipse.gamecomponents.path.*;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class LevelReader {
@@ -15,13 +16,25 @@ public class LevelReader {
 
     private static BufferedReader reader;
     private static long wait = System.nanoTime();
+    private static List<String> commands = new LinkedList<>();
 
     public LevelReader(String fileName) {
         try {
             System.out.println(LEVEL_DIR + fileName);
             reader = new BufferedReader(new FileReader(new File(fileName)));
+
+            String nextLine;
+            while ((nextLine = reader.readLine()) != null) {
+                commands.add(nextLine);
+            }
+
+            reader.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found. Please make sure the file " + fileName + " exists.");
+            e.printStackTrace();
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
             System.exit(0);
         }
     }
@@ -34,92 +47,87 @@ public class LevelReader {
         String next;
         String[] tokens;
 
-        try {
-            // keep reading until the next wait or the end of the file
-            while (true) {
-                next = reader.readLine();
-                if (next == null) return toAdd;
+        // keep reading until the next wait end of stored commands
+        while (true) {
+            if (commands.isEmpty()) return toAdd;
 
-                tokens = next.split(" ");
-                if (tokens[0].equals("") || tokens[0].substring(0, 2).equals("//")) { // reading a comment in the text file
-                    continue;
-                } else if (tokens[0].equals("wait")) {
-                    wait = now + Long.parseLong(tokens[1]) * 1000000;
-                    return toAdd;
-                }
-                String enemy = tokens[0];
-                int xPos = Integer.parseInt(tokens[1]);
-                int yPos = Integer.parseInt(tokens[2]);
+            next = commands.get(0);
+            commands.remove(0);
 
-                VectorPath vectorPath = null;
-
-                switch(tokens[3]) {
-                    case "Down":
-                        vectorPath = new Down();
-                        break;
-                    case "Left":
-                        vectorPath = new Left();
-                        break;
-                    case "Right":
-                        vectorPath = new Right();
-                        break;
-                    case "Up":
-                        vectorPath = new Up();
-                        break;
-                    case "DownRight":
-                        vectorPath = new DownRight();
-                        break;
-                    case "DownLeft":
-                        vectorPath = new DownLeft();
-                        break;
-                    case "UShapeLeft":
-                        vectorPath = new UShapeLeft();
-                        break;
-                    case "UShapeRight":
-                        vectorPath = new UShapeRight();
-                        break;
-                    case "SineDown":
-                        vectorPath = new SineDown();
-                        break;
-                    default:
-                        System.out.println("No vector path was found.");
-                        Thread.dumpStack();
-                        System.exit(0);
-                }
-
-                FirePattern firePattern = null;
-
-                switch (tokens[4]) {
-                    case "FireDown":
-                        firePattern = new FireDown();
-                        break;
-                    case "FireDownLeft":
-                        firePattern = new FireDownLeft();
-                        break;
-                    case "FireDownRight":
-                        firePattern = new FireDownRight();
-                        break;
-                    case "FireTwoSplit":
-                        firePattern = new FireTwoSplit();
-                        break;
-                    case "FireThreeSplit":
-                        firePattern = new FireThreeSplit();
-                        break;
-                    default:
-                        System.out.println("No fire path was found.");
-                        Thread.dumpStack();
-                        System.exit(0);
-                }
-
-                if (enemy.equals("SmallEnemy")) {
-                    toAdd.add(new SmallEnemy(xPos, yPos, vectorPath, firePattern));
-                }
+            tokens = next.split(" ");
+            if (tokens[0].equals("") || tokens[0].substring(0, 2).equals("//")) { // reading a comment in the text file
+                continue;
+            } else if (tokens[0].equals("wait")) {
+                wait = now + Long.parseLong(tokens[1]) * 1000000;
+                return toAdd;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
+            String enemy = tokens[0];
+            int xPos = Integer.parseInt(tokens[1]);
+            int yPos = Integer.parseInt(tokens[2]);
 
-        return null;
+            VectorPath vectorPath = null;
+
+            switch(tokens[3]) {
+                case "Down":
+                    vectorPath = new Down();
+                    break;
+                case "Left":
+                    vectorPath = new Left();
+                    break;
+                case "Right":
+                    vectorPath = new Right();
+                    break;
+                case "Up":
+                    vectorPath = new Up();
+                    break;
+                case "DownRight":
+                    vectorPath = new DownRight();
+                    break;
+                case "DownLeft":
+                    vectorPath = new DownLeft();
+                    break;
+                case "UShapeLeft":
+                    vectorPath = new UShapeLeft();
+                    break;
+                case "UShapeRight":
+                    vectorPath = new UShapeRight();
+                    break;
+                case "SineDown":
+                    vectorPath = new SineDown();
+                    break;
+                default:
+                    System.out.println("No vector path was found.");
+                    Thread.dumpStack();
+                    System.exit(0);
+            }
+
+            FirePattern firePattern = null;
+
+            switch (tokens[4]) {
+                case "FireDown":
+                    firePattern = new FireDown();
+                    break;
+                case "FireDownLeft":
+                    firePattern = new FireDownLeft();
+                    break;
+                case "FireDownRight":
+                    firePattern = new FireDownRight();
+                    break;
+                case "FireTwoSplit":
+                    firePattern = new FireTwoSplit();
+                    break;
+                case "FireThreeSplit":
+                    firePattern = new FireThreeSplit();
+                    break;
+                default:
+                    System.out.println("No fire path was found.");
+                    Thread.dumpStack();
+                    System.exit(0);
+            }
+
+            if (enemy.equals("SmallEnemy")) {
+                toAdd.add(new SmallEnemy(xPos, yPos, vectorPath, firePattern));
+            }
+        }
     }
 }
