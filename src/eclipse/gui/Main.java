326efa1c9049d7e9a4524631a45fbe936d5081ec
Jonathan Gin <jonathangin52@gmail.com>
2018-05-31 18:37:07 -0400

    Stashing changes

--- src/eclipse/gui/Main.java
+++ src/eclipse/gui/Main.java
@@ -13,21 +13,14 @@ import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * @author Jonathan Gin, Justin Reiter, Alex Yang
- */
 public class Main extends Application {
 
     private static Dimension2D dimensions = new Dimension2D(450, 600);
     private final double MINIMUM_WINDOW_WIDTH = 450.0;
     private final double MINIMUM_WINDOW_HEIGHT = 600.0;
-    private final int FRAME_RATE = -1; // -1 to automatically set frame rate
     private Stage stage;
     private Scene scene;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
@@ -59,7 +52,7 @@ public class Main extends Application {
         try {
             GameController game = (GameController) replaceSceneContent("Game.fxml");
             game.setApp(this);
-            game.initGame(FRAME_RATE);
+            game.initGame(-1);
             game.start();
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
@@ -72,13 +65,10 @@ public class Main extends Application {
 
     private Initializable replaceSceneContent(String fxml) throws Exception {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxml));
-        InputStream in = Main.class.getResourceAsStream(fxml);
         loader.setBuilderFactory(new JavaFXBuilderFactory());
         AnchorPane page;
-        try {
+        try (InputStream in = Main.class.getResourceAsStream(fxml)) {
             page = loader.load(in);
-        } finally {
-            in.close();
         }
         scene = new Scene(page, 450, 600);
         stage.setScene(scene);
